68fbaa21994bebd49907d2997019942f97a7e80c
NikanorovS <91907783+NikanorovS@users.noreply.github.com>
2024-03-13 07:56:31 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,8 @@ function addRow() {
     var newRow = document.createElement('tr');
     newRow.innerHTML = `
       <th scope="row">${newRowNumber}</th>
-      <td contenteditable="true" onblur="updateRow(this)">0</td>
-      <td contenteditable="true" onblur="updateRow(this)">0</td>
+      <td contenteditable="true" onblur="updateRow(this)"></td>
+      <td contenteditable="true" onblur="updateRow(this)"></td>
       <td></td>
       <td>
         <button class="btn btn-success" onclick="addRow()">+</button>
